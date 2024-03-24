79680dba77cf08adbecedbf6dc7677b37720460d
winarchy <83733357+winarchy@users.noreply.github.com>
2024-03-24 13:44:14 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam repellendus aliquam in, obcaecati maxime dolorem similique tempore esse sint eos deserunt exercitationem possimus natus corporis suscipit ratione quod error quos?</p>
 </body>
 </html>
