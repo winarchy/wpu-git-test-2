0782016b6effa392a1afd85a364cd8abdd6b745e
winarchy <83733357+winarchy@users.noreply.github.com>
2024-03-24 13:50:37 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam repellendus aliquam in, obcaecati maxime dolorem similique tempore esse sint eos deserunt exercitationem possimus natus corporis suscipit ratione quod error quos?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
